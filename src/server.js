35bedae06c379a6681fb62647545f260d4c55add
Arthur Melkonyan <arthur@artmelkon.me>
2022-01-08 08:40:15 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./graphql/schema");
+const resolvers = require("./graphql/resolvers");
+const TrackAPI = require("./datasources/track-api");
+
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
+
+  const PORT = process.env.PORT || 4000;
+  const { url, port } = await server.listen(PORT);
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${PORT}
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer(typeDefs, resolvers);
